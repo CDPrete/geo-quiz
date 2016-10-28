3891d7f13881aa2b36d087f34a5b8c73ab14961c
Cosimo Damiano Prete <cosimo.prete.damiano@gmail.com>
2016-10-28 23:34:14 +0200

    Chapter 06 completed.

--- app/src/main/java/com/bignerdranch/android/geoquiz/controller/CheatActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/controller/CheatActivity.java
@@ -1,10 +1,14 @@
 package com.bignerdranch.android.geoquiz.controller;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -41,6 +45,27 @@ public class CheatActivity extends AppCompatActivity {
                 }
                 mAnswerTextView.setText(answerRes);
                 setAnswerShownResult(true);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+                    Animator animator =
+                            ViewAnimationUtils.createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                    animator.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+
+                            mAnswerTextView.setVisibility(View.VISIBLE);
+                            mShowAnswer.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    animator.start();
+                } else {
+                    mAnswerTextView.setVisibility(View.VISIBLE);
+                    mShowAnswer.setVisibility(View.INVISIBLE);
+                }
             }
         });
     }
